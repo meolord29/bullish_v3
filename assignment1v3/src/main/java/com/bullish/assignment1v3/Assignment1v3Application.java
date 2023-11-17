7e9b039222fd42d72d18f74a4c6cf82ef60549b3
meolord29 <53997283+meolord29@users.noreply.github.com>
2023-11-17 09:28:40 +0800

    remade

--- assignment1v3/src/main/java/com/bullish/assignment1v3/Assignment1v3Application.java
+++ assignment1v3/src/main/java/com/bullish/assignment1v3/Assignment1v3Application.java
@@ -1,13 +1,13 @@
-package com.bullish.assignment1_v3;
+package com.bullish.assignment1v3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment1V3Application {
+public class Assignment1v3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Assignment1V3Application.class, args);
+		SpringApplication.run(Assignment1v3Application.class, args);
 	}
 
 }
--- assignment1v3/src/test/java/com/bullish/assignment1v3/Assignment1v3ApplicationTests.java
+++ assignment1v3/src/test/java/com/bullish/assignment1v3/Assignment1v3ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bullish.assignment1_v3;
+package com.bullish.assignment1v3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Assignment1V3ApplicationTests {
+class Assignment1v3ApplicationTests {
 
 	@Test
 	void contextLoads() {
