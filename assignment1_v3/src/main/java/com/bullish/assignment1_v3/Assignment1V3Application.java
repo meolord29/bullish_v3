a83374dfda5c13b398c1c08cb619337befb81a71
meolord29 <53997283+meolord29@users.noreply.github.com>
2023-11-17 09:22:17 +0800

    setting up spring dependancies
    - web
    - lombok
    - security
    - JPA
    - H2
    - devTools
    - REST Docs

---
+++ assignment1_v3/src/main/java/com/bullish/assignment1_v3/Assignment1V3Application.java
@@ -0,0 +1,13 @@
+package com.bullish.assignment1_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1V3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1V3Application.class, args);
+	}
+
+}
---
+++ assignment1_v3/src/test/java/com/bullish/assignment1_v3/Assignment1V3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullish.assignment1_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1V3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
