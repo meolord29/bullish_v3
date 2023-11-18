af04ca016b89f3c95acfe6c4839479d2d9ef107e
meolord29 <53997283+meolord29@users.noreply.github.com>
2023-11-18 13:23:33 +0800

    Create ProductRepository.java

---
+++ assignment1v3/src/main/java/com/bullish/assignment1v3/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.bullish.assignment1v3.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bullish.assignment1v3.model.store.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long>{
+	
+	Optional<Product> findByUsername(String username);
+}
