5d861633ce07491aee9c57ffb8920407007e152a
meolord29 <53997283+meolord29@users.noreply.github.com>
2023-11-22 17:35:44 +0800

    updated calculation method

--- assignment1v3/src/main/java/com/bullish/assignment1v3/service/BasketCalculateService.java
+++ assignment1v3/src/main/java/com/bullish/assignment1v3/service/BasketCalculateService.java
@@ -4,18 +4,38 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.bullish.assignment1v3.model.store.Basket;
 import com.bullish.assignment1v3.model.store.Product;
 import com.bullish.assignment1v3.service.contracts.BasketCalculate.BasketSumCalculatable;
+import com.bullish.assignment1v3.service.contracts.BasketCalculate.BasketSumDiscountable;
+import com.bullish.assignment1v3.service.contracts.BasketCalculate.BasketSumOwedCalculatable;
 
 @Service
-public class BasketCalculateService implements BasketSumCalculatable, BasketSumOwedCalculatable {
+public class BasketCalculateService implements BasketSumCalculatable, BasketSumOwedCalculatable, BasketSumDiscountable {
 
     @Autowired
     private ProductService productService;
+
+    private ConcurrentHashMap<String, ConcurrentHashMap<String, Double>> getHashmapOfBasket(List<Basket> basket) {
+
+        ConcurrentHashMap<String, ConcurrentHashMap<String, Double>> basketHashMap = 
+        new ConcurrentHashMap<String, ConcurrentHashMap<String, Double>>();
+
+        for(Basket basketItem : basket){
+            String productName = basketItem.getProductName();
+            Double total = 0d + basketItem.getTotal();
+            Double price = productService.readProduct(basketItem.getProductName()).get().getPrice();
+            ConcurrentHashMap<String, Double> productInformation = new ConcurrentHashMap<String, Double>();
+
+            productInformation.put("total", total);
+            productInformation.put("price", price);
+
+            basketHashMap.put(productName, productInformation);
+        }
+        return basketHashMap;
+    }
 
     @Override
     public Double calculateBasketSum(List<Basket> basket) {
@@ -39,6 +59,14 @@ public class BasketCalculateService implements BasketSumCalculatable, BasketSumO
         return totalSum;
     }
     
-
+    @Override
+    public Double CalculateDiscounted(List<Basket> basket){
+        Double totalSum = 0d;
+        for(Basket basketItem : basket){
+            Product product = productService.readProduct(basketItem.getProductName()).get();
+            totalSum += product.getPrice() * basketItem.getTotal();
+        }
+        return totalSum;
+    }
 
 }
